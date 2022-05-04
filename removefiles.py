403a039c5fbc446af2457294a63d8c1099cb3fb2
chickenDanielDaniel <88032246+chickenDanielDaniel@users.noreply.github.com>
2022-05-04 16:37:48 +0800

    Add files via upload

---
+++ removefiles.py
@@ -0,0 +1,13 @@
+from importlib.resources import path
+import time
+import os
+import shutil
+path = input("Please enter your file")
+days = input("Please enter ")
+time.time()
+os.path.exists(path)
+os.walk(path)
+os.path.join()
+os.stat(path)
+os.remove(path)
+shutil.rmtree()
